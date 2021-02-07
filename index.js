54bfb445a60c9a3f9d12adec6af01faab6832c98
Lucas <rocha99alvarez@gmail.com>
2021-02-07 04:12:48 -0300

    first commit

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const app     = express();
 
+const port = 5000;
+
+
 app.get('*', (req , res) => {
   console.log('Hola Mundo! Mi Primera Aplicacion Serveless');
   res.send({mensaje: "Emmita en la Web"})
 })
 
-module.exports = app;
+app.listen(port, () =>{
+  console.log('hola');
+})
+
+/*module.exports = app;*/
